1980efa54550492327a23b403dd4f8d734aeae66
frost <Bear-Frost@users.noreply.github.com>
2024-10-20 00:46:35 -0700

    chore: add a utility function to set a default position to the element

--- src/utils/defaultstyle.ts
+++ src/utils/defaultstyle.ts
@@ -8,4 +8,24 @@ const colorTheme: IColorTheme = {
   secondary: '#BFFF00',
 } as const;
 
-export { colorTheme };
+/**
+ * Sets a default position of `relative` to an element if its position is currently set to `static`.
+ * @param {HTMLElement} element - The element for which to set a default position.
+ * @return void
+ * @example
+ * const element = document.getElementById('element');
+ *
+ * // Sets the default position to 'relative' if the element's position is 'static'
+ * setElementDefaultPosition(element);
+ */
+
+const setElementDefaultPosition = (element: HTMLElement): void => {
+  const elementPosition = window
+    .getComputedStyle(element)
+    .getPropertyValue('position');
+  if (elementPosition === 'static') {
+    element.style.position = 'relative';
+  }
+};
+
+export { setElementDefaultPosition, colorTheme };
