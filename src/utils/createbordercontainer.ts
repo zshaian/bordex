74c153a583a95837e1776f6648562a54f87602bb
frost <Bear-Frost@users.noreply.github.com>
2024-10-20 21:37:29 -0700

    chore(utility): add a function that will act as a border container element

---
+++ src/utils/createbordercontainer.ts
@@ -0,0 +1,31 @@
+import { getComputedStyleValue } from './getcomputedstylevalue';
+import { getElementPosition } from './getelementposition';
+
+/**
+ * Creates a new `div` element that acts as a border container for the provided element.
+ * The newly created `div` will have styles such as `width`, `borderRadius`, and `position`
+ * derived from the provided element's computed styles.
+ *
+ * @param {HTMLElement} element - The HTML element for which to create a border container.
+ * @returns {HTMLDivElement} The newly created `div` element styled as a border container.
+ *
+ * @example
+ * const element = document.getElementById('element');
+ * const borderContainer = borderContainerElement(element);
+ *
+ * document.body.appendChild(borderContainer);
+ */
+function borderContainerElement(element: HTMLElement): HTMLDivElement {
+  const borderContainerElement = document.createElement('div');
+  const borderContainerElementStyle = {
+    width: 'max-content',
+    borderRadius: getComputedStyleValue(element, 'border-radius'),
+    position: getElementPosition(element),
+  };
+
+  Object.assign(borderContainerElement.style, borderContainerElementStyle);
+
+  return borderContainerElement;
+}
+
+export { borderContainerElement };
