e246194e286ed38316004cca4d3d96ddbfc82c25
frost <Bear-Frost@users.noreply.github.com>
2024-10-23 02:30:25 +0800

    feat(api): add a function to add a thin border to an element.

---
+++ src/addthinborder.ts
@@ -0,0 +1,101 @@
+import { COLOR_THEME } from './utils/defaultstyle';
+import { validateHTMLElement } from './utils/validatehtmlelement';
+
+type ThinBorderSides =
+  | 'top'
+  | 'right'
+  | 'bottom'
+  | 'left'
+  | 'horizontal'
+  | 'vertical';
+
+interface IThinBorder {
+  color?: string;
+  side?: ThinBorderSides;
+  outset?: string;
+}
+
+/**
+ * Add a thin border to an element.
+ * @param {HTMLElement} element - The element to add the thin border.
+ * @param {IThinBorder} [borderOptions={}] - The border options (`color`, `side`, `outset`).
+ * @throws {Error} Will throw an error if the first argument is not an HTML element, or if one the borderSide is not valid.
+ * @returns {void}
+ * @example
+ * const element = document.getElementById('element');
+ * const anotherElement = document.getElementById('anotherElement');
+ *
+ * // Add thin border to the element with default options
+ * addFancytBorder(element);
+ *
+ * // Add a thin border to the element with a provided options
+ * addFancyBorder(anotherElement, { color: 'yellow' , side: 'horizontal', outset: '5px' });
+ */
+function addThinBorder(
+  element: HTMLElement,
+  borderOptions: IThinBorder = {},
+): void {
+  validateHTMLElement(element);
+
+  const {
+    color = COLOR_THEME.secondary, // use the secondary color cause it's more bright.
+    side = 'left',
+    outset = '0px',
+  } = borderOptions;
+
+  const validBorderSides = [
+    'top',
+    'right',
+    'bottom',
+    'left',
+    'horizontal',
+    'vertical',
+  ];
+
+  // validate the side property of borderOptions.
+  if (!validBorderSides.includes(side)) {
+    throw new Error(
+      `Invalid side value: ${side}. Must be one of ${validBorderSides.join(', ')}`,
+    );
+  }
+
+  const isThinBorderOnSide = ['left', 'right', 'horizontal'].includes(side);
+  const borderImageSlice = getBorderImageSlice(side);
+
+  const thinBorderStyle = {
+    borderImageSource: `linear-gradient(${isThinBorderOnSide ? 'to bottom' : 'to left'},rgba(0,0,0,0) 1%, ${color} 50%, rgba(0,0,0,0) 99%)`,
+    borderImageOutset: `${isThinBorderOnSide ? `0 ${outset}` : `${outset} 0`}`,
+    borderImageWidth: '1.5px', // width of the border, maybe adjust it later if it's too thin.
+    borderImageSlice,
+  };
+
+  Object.assign(element.style, thinBorderStyle);
+}
+
+/**
+ * This is going to return a value on where or what side to put the border.
+ *
+ * @param {ThinBorderSides} side - The side to put the borde.
+ * @returns {string} The `border-image-slice` CSS property value.
+ */
+// TODO: Allow the option to apply a thin border to all four sides of the element.
+function getBorderImageSlice(side: ThinBorderSides): string {
+  switch (side) {
+    case 'left':
+      return '0 0 0 100%';
+    case 'right':
+      return '0 100% 0 0';
+    case 'top':
+      return '100% 0 0 0';
+    case 'bottom':
+      return '0 0 100% 0';
+    case 'horizontal':
+      return '0 100% 0 100%';
+    case 'vertical':
+      return '100% 0 100% 0';
+    default:
+      return '0 0 0 100%'; // Fallback case (though it should not hit this due to validation)
+  }
+}
+
+export { addThinBorder };
