f77ee0a84b17f89411c1b937947a77d43a5e46bc
frost <Bear-Frost@users.noreply.github.com>
2024-10-25 03:04:25 +0800

    refactor: create a function that will set a default style properties to the element
    Create a function that will just restart all the style properties values
    that have been copied in the borderContainerElement

--- src/utils/createbordercontainer.ts
+++ src/utils/createbordercontainer.ts
@@ -1,3 +1,5 @@
+import { clearElementBackground } from './clearelementbackground';
+import { copySourceBackground } from './copybaseelementbackground';
 import { getComputedStyleValue } from './getcomputedstylevalue';
 import { getElementPosition } from './getelementposition';
 
@@ -17,22 +19,38 @@ import { getElementPosition } from './getelementposition';
  */
 function borderContainerElement(element: HTMLElement): HTMLDivElement {
   const borderContainerElement = document.createElement('div');
+
   const borderContainerElementStyle = {
     // FIXME: Find a way to set the right width to the border container instead of just setting it to max-content
     width: 'max-content',
     borderRadius: getComputedStyleValue(element, 'border-radius'),
     position: getElementPosition(element),
+    zIndex: getComputedStyleValue(element, 'z-index'),
   };
-
-  /*
-    sets the position of element to relative, because the border container element, already
-    have the same position value of the element.
+  /* reset all the style properties of the element, that get copied,
+     into the borderContainerElmement.
   */
-  element.style.position = 'relative';
+  resetCopiedElementStyles(element, borderContainerElement);
 
   Object.assign(borderContainerElement.style, borderContainerElementStyle);
 
   return borderContainerElement;
+}
+
+// TODO: document this function.
+function resetCopiedElementStyles(
+  element: HTMLElement,
+  baseElement: HTMLElement,
+): void {
+  /*
+    sets the position and z-index of an element to relative, because the border container element already
+    have the same position and z-index value of the element.
+  */
+  element.style.position = 'relative';
+  element.style.zIndex = 'auto';
+
+  copySourceBackground(element, baseElement); // copy the background of element to the border container element.
+  clearElementBackground(element); // clear the background of the element (cause we just copied it to the border container element).
 }
 
 export { borderContainerElement };
