de8d0b2d9af53feb9aa5b52a43f97465237173cc
frost <Bear-Frost@users.noreply.github.com>
2024-10-25 21:13:19 +0800

    docs: add documentation to the function using JSDoc

--- src/utils/clearelementbackground.ts
+++ src/utils/clearelementbackground.ts
@@ -1,4 +1,8 @@
-//TODO: document this function
+/**
+ * this will set the background of the element to transparent.
+ * @param {HTMLElement} element - The element that you want to set the background to transparent.
+ * @returns {void} No return value.
+ */
 function clearElementBackground(element: HTMLElement): void {
   element.style.backgroundColor = 'transparent';
   element.style.background = 'none';
