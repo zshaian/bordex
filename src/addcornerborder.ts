4bca5da4145cf528ff14eab9dacaff8b64f6fddd
frost <Bear-Frost@users.noreply.github.com>
2024-10-25 21:31:15 +0800

    docs: add and document the individual function using JSDoc

--- src/addcornerborder.ts
+++ src/addcornerborder.ts
@@ -17,7 +17,13 @@ interface IBorderRadius {
 
 type ICornerBorderOptions = IShorthandBorder & IPositions & IBorderRadius;
 
-// TODO: document this function
+/**
+ * Adds a corner border to an element.
+ *
+ * @param {HTMLElement} element - The element to which the corner border will be added.
+ * @param {IShorthandBorderOptions} [borderOptions={}] - Options for the border, including width, color, and style.
+ * @returns {void} - No return value.
+ */
 function addCornerBorder(
   element: HTMLElement,
   borderOptions: IShorthandBorderOptions = {},
@@ -36,7 +42,15 @@ function addCornerBorder(
   borderContainer.append(element);
 }
 
-// TODO: document this function
+/**
+ * Creates and appends corner borders to the specified border container.
+ * This function generates corner border options for each corner and appends the corresponding
+ * corner border elements to the container.
+ *
+ * @param {HTMLDivElement} borderContainer - The container to which the corner borders will be appended.
+ * @param {string} cornerBorder - The border style string to apply to the corners.
+ * @returns {void} - No return value.
+ */
 function createAndAppendCornerBorder(
   borderContainer: HTMLDivElement,
   cornerBorder: string,
@@ -53,7 +67,15 @@ function createAndAppendCornerBorder(
   );
 }
 
-// TODO: document this function
+/**
+ * Creates an options object for corner borders based on the specified vertical and horizontal positions.
+ * This function returns an object that includes border styles and position properties for a specific corner.
+ *
+ * @param {'top' | 'bottom'} vertical - The vertical position of the corner (top or bottom).
+ * @param {'left' | 'right'} horizontal - The horizontal position of the corner (left or right).
+ * @param {string} borderStyle - The style of the border to apply to the corners.
+ * @returns {ICornerBorderOptions} - An object containing corner border options.
+ */
 function createCornerBorderOptions(
   vertical: 'top' | 'bottom',
   horizontal: 'left' | 'right',
@@ -69,7 +91,13 @@ function createCornerBorderOptions(
   };
 }
 
-// TODO: document this function
+/**
+ * Creates a corner border element with the specified border options.
+ * This function generates a div element styled as a corner border based on the provided options.
+ *
+ * @param {ICornerBorderOptions} cornerBorderOptions - The options for styling the corner border.
+ * @returns {HTMLDivElement} - The created corner border element.
+ */
 function createCornerBorder(
   cornerBorderOptions: ICornerBorderOptions,
 ): HTMLDivElement {
