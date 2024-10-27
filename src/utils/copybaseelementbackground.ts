24f04d2cd37c95c5cc5df701fe2d3028d775c042
frost <Bear-Frost@users.noreply.github.com>
2024-10-27 21:01:30 +0800

    refactor: simplify background setting in copySourceBackground function

--- src/utils/copybaseelementbackground.ts
+++ src/utils/copybaseelementbackground.ts
@@ -12,17 +12,11 @@ function copySourceBackground(
   sourceElement: HTMLElement,
   targetElement: HTMLElement,
 ): void {
-  const sourceElementBgc = getComputedStyleValue(
-    sourceElement,
-    'background-color',
-  ) as string;
-
   const sourceElementBg = getComputedStyleValue(
     sourceElement,
     'background',
   ) as string;
 
-  targetElement.style.backgroundColor = sourceElementBgc;
   targetElement.style.background = sourceElementBg;
 }
 
