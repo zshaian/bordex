ac3337bd77404398863d3301245d0949ae51704f
frost <Bear-Frost@users.noreply.github.com>
2024-10-30 09:02:02 +0800

    feat: set up main entry point for library exports

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,12 @@
-interface ISolidBorderElementStyle {
-  borderStyle: string;
-  borderWidth: string;
-  borderColor: string;
-}
-
-type SolidBorderElementOptions = Pick<
-  ISolidBorderElementStyle,
-  'borderWidth' | 'borderColor'
->;
-
-function createSolidBorder(
-  element: HTMLElement,
-  borderOptions?: SolidBorderElementOptions,
-): void {
-  const solidBorderElementStyle: ISolidBorderElementStyle = {
-    borderStyle: 'solid',
-    borderWidth: '5px',
-    borderColor: 'rgb(0,0,0)',
-    ...borderOptions,
-  };
-  Object.assign(element.style, solidBorderElementStyle);
-  return;
-}
-
-const element = document.getElementById('element') as HTMLElement;
-createSolidBorder(element);
+export {
+  addBlurBorder,
+  addCornerBorder,
+  addDisjointedBorder,
+  addFancyBorder,
+  addGenericBorder,
+  addGradientBorder,
+  addInsetBorder,
+  addOverlapBorder,
+  addStripeBorder,
+  addThinBorder,
+} from './borderstyles';
