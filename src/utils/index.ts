94ece5cf24617cfef565c4e06ab37a604f835292
shaian <zshaian@users.noreply.github.com>
2024-11-17 15:36:29 +0800

    chore: remove unused code

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,6 +2,5 @@ export { capitalizeFirstLetter } from './capitalizefirstletter';
 export { borderContainerElement } from './createbordercontainer';
 export { COLOR_THEME, BORDER_SIZE } from './defaultstyle';
 export { getComputedStyleValue } from './getcomputedstylevalue';
-export { getElementPosition } from './getelementposition';
 export { insertAfterElement } from './insertafterelement';
 export { validateHTMLElement } from './validatehtmlelement';
