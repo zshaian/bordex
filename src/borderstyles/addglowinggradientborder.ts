638362bc8448cddf534b948250d6cc9f09be6957
shaian <zshaian@users.noreply.github.com>
2024-11-18 14:18:54 +0800

    docs: add a JSDoc comment for the addGlowingGradientBorder function

--- src/borderstyles/addglowinggradientborder.ts
+++ src/borderstyles/addglowinggradientborder.ts
@@ -2,6 +2,26 @@ import { IGradientBorderOptions, CSSStyles } from '../types';
 import { COLOR_THEME } from '../utils';
 import { addGradientBorder } from './addgradientborder';
 
+/**
+ * Add a glowing gradient border to an element.
+ * @param {HTMLElement} element - The element to add a gradient border to.
+ * @param {IGradientBorderOptions} [borderOptions={}] - The border options (`angle`, `colors`, `borderWidth`, etc.).
+ * @param {string} [className] - Optional class name to apply custom CSS styling to the container.
+ * @throws {Error} Will throw an error if the first argument is not an HTML element.
+ * @returns {HTMLDivElement} The container element wrapping the original element with the glowing gradient border applied.
+ *
+ * @example
+ * const element = document.getElementById('element');
+ * const anotherElement = document.getElementById('anotherElement');
+ *
+ * // Add glowing gradient border to the element with default options
+ * addGradientBorder(element);
+ *
+ * // Add a gradient border to the element with a gradient angle of 20deg,
+ * // with a glowing gradient color of "red", "orange", and "purple",
+ * // and a border width of 10px
+ * addGradientBorder(anotherElement, { angle: '20deg',colors:['red', 'orange', 'purple'], borderWidth: '10px' });
+ */
 function addGlowingGradientBorder(
   element: HTMLElement,
   borderOptions: IGradientBorderOptions = {},
