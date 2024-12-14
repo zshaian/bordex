108814a335889f3ba8dff42bc1811577a3e1b5a2
shaian <zshaian@users.noreply.github.com>
2024-12-14 11:24:12 +0800

    feat: add the blurBorder component

---
+++ src/components/BlurBorder.tsx
@@ -0,0 +1,34 @@
+import { forwardRef, useMemo } from 'react';
+
+import type { BlurBorderProps } from '../types/type';
+import { DEFAULT_COLOR_THEME, DEFAULT_BORDER_SIZE } from '../themes/theme';
+
+const defaultBlurBorderProps = {
+  borderWidth: DEFAULT_BORDER_SIZE.sm,
+  borderBlur: '20px',
+  borderColor: DEFAULT_COLOR_THEME.primary,
+} satisfies BlurBorderProps;
+
+const BlurBorder = forwardRef<HTMLDivElement, BlurBorderProps>((props, ref) => {
+  const { borderBlur, borderWidth, borderColor, ...rest } = {
+    ...defaultBlurBorderProps,
+    ...props,
+  };
+
+  const blurBorderStyleValue = `${borderBlur} ${borderWidth} ${borderColor}`;
+
+  const blurBorderStyle: React.CSSProperties = useMemo(
+    () => ({
+      boxShadow: `0px 0px  ${blurBorderStyleValue}`,
+    }),
+    [blurBorderStyleValue],
+  );
+
+  return (
+    <div ref={ref} {...rest} style={blurBorderStyle}>
+      {props.children}
+    </div>
+  );
+});
+
+export default BlurBorder;
