f4dae175f1ca9692272c5019080047e9f69d189d
shaian <zshaian@users.noreply.github.com>
2025-02-22 10:04:37 +0800

    chore: addd the index or entry point for the library

---
+++ src/components/index.tsx
@@ -0,0 +1,10 @@
+export { default as BlurBorder } from './BlurBorder';
+export { default as CornerBorder } from './CornerBorder';
+export { default as DisjointedBorder } from './DisjointedBorder';
+export { default as FancyBorder } from './FancyBorder';
+export { default as GenericBorder } from './GenericBorder';
+export { default as GradientBorder } from './GradientBorder';
+export { default as InsetBorder } from './GradientBorder';
+export { default as OverlapBorder } from './OverlapBorder';
+export { default as StripeBorder } from './StripeBorder';
+export { default as ThinBorder } from './ThinBorder';
---
+++ src/index.tsx
@@ -0,0 +1,12 @@
+export {
+  BlurBorder,
+  CornerBorder,
+  DisjointedBorder,
+  FancyBorder,
+  GenericBorder,
+  GradientBorder,
+  InsetBorder,
+  OverlapBorder,
+  StripeBorder,
+  ThinBorder,
+} from './components';
